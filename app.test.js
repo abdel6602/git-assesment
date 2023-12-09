451bb13ab474caf77b80d6a874e53b1cc3bc0d2d
abdel6602 <abdelrahmangamal66002@gmail.com>
2023-12-10 01:28:08 +0200

    added new function

--- app.test.js
+++ app.test.js
@@ -1,6 +1,9 @@
-const add = require('./app');
+const {add, subtract} = require('./app');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
 
+test('subtracts', () => {
+    expect(subtract(3, 2)).toBe(1)
+})
